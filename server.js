600351e1f14755abaa9e5df41aecc2286a2aef6b
Danillo Souza Pereira <danillodesouza03@gmail.com>
2024-12-01 13:56:18 -0300

    Criar esboço das rotas CRUD

---
+++ server.js
@@ -0,0 +1,29 @@
+import { fastify } from 'fastify'
+
+const app = fastify()
+
+app.get('/', () => {
+  return 'API no ar!'
+})
+
+app.get('/videos', () => {
+  return 'GET para ler'
+})
+
+app.post('/videos', () => {
+  return 'POST para criar'
+})
+
+app.put('/videos/:id', () => {
+  return 'PUT para editar'
+})
+
+app.delete('/video/:id', () => {
+  return 'DELETE para deletar'
+})
+
+app.listen({
+  port: 3333
+}).then(() => {
+  console.log('Rodando na porta 3333')
+})
